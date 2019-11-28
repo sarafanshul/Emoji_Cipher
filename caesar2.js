7abd340b34b1c3bba2c8b805e8876a0aa0c9255e
incognitee <saraf2029@gmail.com>
2019-11-28 18:27:50 +0530

    fully working

--- caesar2.js
+++ caesar2.js
@@ -1,22 +1,3 @@
-// implementation
-
-// < !DOCTYPE html >
-//     <html>
-//         <head>
-//             <title>Caesar Cipher</title>
-//         </head>
-//         <body>
-//             The shift (number from -26 to 26): <Br>
-//                 <input type="tel" id="num"><br><br>
-//                     Text to encode / decode: <br>
-//                         <input type="text" id="txt"><br><br>
-
-//                             <button onclick="caesar()">Encode</button>
-//                             <button onclick="decaesar()">Decode</button>
-  
-  
-// 	</body>
-// </html>
 
 function caesar() {
     var num = parseInt(document.getElementById("num").value)
@@ -54,16 +35,101 @@ function caesar() {
             }
         }
     }
-    // else {
-    //     document.write("Number must be from -26 to 26, try again :)")
-    // }
-    document.write(String.fromCharCode.apply(null, x))
+    const dict = {
+        a: '&#128512;',
+        b: '&#128513;',
+        c: '&#128514;',
+        d: '&#128515;',
+        e: '&#128516;',
+        f: '&#128517;',
+        g: '&#128518;',
+        h: '&#128519;',
+        i: '&#128520;',
+        j: '&#128521;',
+        k: '&#128522;',
+        l: '&#128523;',
+        m: '&#128524;',
+        n: '&#128525;',
+        o: '&#128526;',
+        p: '&#128527;',
+        q: '&#128528;',
+        r: '&#128529;',
+        s: '&#128530;',
+        t: '&#128531;',
+        u: '&#128532;',
+        v: '&#128533;',
+        w: '&#128534;',
+        x: '&#128535;',
+        y: '&#128536;',
+        z: '&#128537;',
+    }
+    ans = String.fromCharCode.apply(null, x)
+    ans = ans.toLowerCase()
+    out = ''
+    for (let i in ans) {
+        // console.log(dict[ans[i]])
+        out += dict[ans[i]]
+    }
+    document.getElementById('answer').innerHTML = 'Encoded Crypto ->   ' + out
+}
+
+function getSymbols(string) {
+    var index = 0;
+    var length = string.length;
+    var output = [];
+    for (; index < length; ++index) {
+        var charCode = string.charCodeAt(index);
+        if (charCode >= 0xD800 && charCode <= 0xDBFF) {
+            charCode = string.charCodeAt(index + 1);
+            if (charCode >= 0xDC00 && charCode <= 0xDFFF) {
+                output.push(string.slice(index, index + 2));
+                ++index;
+                continue;
+            }
+        }
+        output.push(string.charAt(index));
+    }
+    return output;
 }
 
 function decaesar() {
     var num = parseInt(document.getElementById("num").value) * -1
-    var txt = document.getElementById("txt").value;
+    var out = document.getElementById("txt").value;
     var x = [];
+    const dict = {
+        '😀': 'a',
+        '😁': 'b',
+        '😂': 'c',
+        '😃': 'd',
+        '😄': 'e',
+        '😅': 'f',
+        '😆': 'g',
+        '😇': 'h',
+        '😈': 'i',
+        '😉': 'j',
+        '😊': 'k',
+        '😋': 'l',
+        '😌': 'm',
+        '😍': 'n',
+        '😎': 'o',
+        '😏': 'p',
+        '😐': 'q',
+        '😑': 'r',
+        '😒': 's',
+        '😓': 't',
+        '😔': 'u',
+        '😕': 'v',
+        '😖': 'w',
+        '😗': 'x',
+        '😘': 'y',
+        '😙': 'z',
+    }
+    var symbols = getSymbols(out)
+    var txt = ''
+
+    symbols.forEach(function(symbol){
+        txt += dict[symbol]
+    })
     num %= 26
     if (num >= -26 && num <= 26) {
         for (i = 0; i < txt.length; i++) {
@@ -95,5 +161,6 @@ function decaesar() {
             }
         }
     }
-    document.write(String.fromCharCode.apply(null, x))
+    ans = String.fromCharCode.apply(null, x)
+    document.getElementById('answer').innerHTML = 'Decoded Crypto ->   ' + ans
 }
